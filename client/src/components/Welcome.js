4172fd3d466163a1b3060018ec9d67fe20c08e13
GabytaDev <gabyta.loyola@gmail.com>
2022-04-25 20:55:19 -0300

    add style grid and card

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,18 +1,49 @@
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-import {Loader} from "./Loader";
+import { Loader } from "./Loader";
 import "./Welcome.scss";
 
-const Welcome = ()=>{
+const Welcome = () => {
+    const connectWallet = () => {
+
+    }
     return (
         <div className="content-welcome">
-            <div className="content-inner"> 
+            <div className="content-inner">
                 <div className="content-title">
-                    <h1 className="title-welcome">Send Crypto <br/> across the world</h1>
+                    <h1 className="title-welcome">Send Crypto <br /> across the world</h1>
                     <p className="subtitle-welcome">Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.</p>
+                    <button type="button" onClick={connectWallet}
+                        className="button-conect-wallet">
+                        <p className="text-button-conect">Connect Wallet</p>
+                    </button>
+                    <div className="content-grid">
+                        <div >Reliability</div>
+                        <div >Security</div>
+                        <div>Ethereum</div>
+                        <div>Web 3.0</div>
+                        <div>Low Fees</div>
+                        <div>Blockchain</div>
+                    </div>
                 </div>
             </div>
+
+            <div className="content-inner-right">
+                <div className="content-card">
+                    <div className="content-inner-card">
+                        <div className="content-item"> 
+                        <div className="content-icon">
+                        <SiEthereum className="icon-ethereum" />
+                        </div>
+                        
+                        </div>
+                       
+                    </div>
+
+                </div>
+            </div>
+
         </div>
     )
 }

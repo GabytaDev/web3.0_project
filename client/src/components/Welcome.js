1a7890c032d0bc863ac4f2ce561f41a1cea1bd42
GabytaDev <gabyta.loyola@gmail.com>
2022-04-25 11:30:41 -0300

    add estyles title welcome

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,6 +1,19 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+import {Loader} from "./Loader";
+import "./Welcome.scss";
+
 const Welcome = ()=>{
     return (
-        <div>Welcome</div>
+        <div className="content-welcome">
+            <div className="content-inner"> 
+                <div className="content-title">
+                    <h1 className="title-welcome">Send Crypto <br/> across the world</h1>
+                    <p className="subtitle-welcome">Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
